49da88cc995b83266581a258278be9e0e5f54237
Ananthu R V <ananthurv666@hotmail.com>
2015-09-14 13:27:06 +0530

    #05 Ananthu|gaurav : added code snippet

--- src/createQuestion/create_question.js
+++ src/createQuestion/create_question.js
@@ -1,4 +1,14 @@
 
+function markAsCode(){
+    var textComponent = document.getElementById('question');
+    var selectedText;
+    var startPos = textComponent.selectionStart;
+    var endPos = textComponent.selectionEnd;
+    selectedText = textComponent.value.substring(startPos, endPos)
+    textComponent.value =  textComponent.value.replace(selectedText,"<code>"+selectedText+"</code>")
+
+}
 $(document).ready(function() {
+    $("#markAsCode").on('click',markAsCode)
 
 })
