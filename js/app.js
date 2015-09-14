08d6d7feda35e7280bf7c39617cb55cce6b1074c
Ananthu R V <ananthurv666@hotmail.com>
2015-09-15 00:52:17 +0530

    #05 gaurav| move window maximization in app.js and removed maximizewindow.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var gui = require('nw.gui');
 win = gui.Window.get();
 var nativeMenuBar = new gui.Menu({ type: "menubar" });
 try {
+    win.maximize();
     nativeMenuBar.createMacBuiltin("My App");
     win.menu = nativeMenuBar;
 } catch (ex) {
