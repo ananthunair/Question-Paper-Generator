af9e2bf7db8bd40bbf0491af0ceea3a1e9dd2483
dollyg <dollyg>
2015-09-15 12:51:09 +0530

    Suparna | Dolly : Refactored script for runUnitTests.

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -2,9 +2,7 @@ touch ~/.bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.bash_profile
 nvm install 4.0
-echo export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin > ~/.bash_profile
-source ~/.bash_profile
-
+export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin
 npm install
 npm test
 tar -cvf QPG.tar .
