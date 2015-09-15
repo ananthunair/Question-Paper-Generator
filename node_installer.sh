33331f35d805f8196d742afcdbd844248650995b
Gaurav Deshkar <gauravd@INgauravd.local>
2015-09-15 11:50:20 +0530

    fix node installation scrip

--- node_installer.sh
+++ node_installer.sh
@@ -2,4 +2,4 @@ touch ~/.bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.bash_profile
 nvm install 4.0
-export PATH =$PATH:~/.nvm/versions/node/v4.0.0/bin
+export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin
