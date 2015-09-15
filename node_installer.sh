bcb3baf5b08c8fc528bfd3e49ebdf1a737f8d53a
dollyg <dollyg>
2015-09-15 12:25:38 +0530

    add export in bash_profile

--- node_installer.sh
+++ node_installer.sh
@@ -2,4 +2,5 @@ touch ~/.bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.bash_profile
 nvm install 4.0
-export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin
+echo export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin > ~/.bash_profile
+source ~/.bash_profile
