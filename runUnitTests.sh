00153c311dd24076685070e510e9da899813ed93
dollyg <dollyg>
2015-09-15 15:01:22 +0530

    added lebel to the artifact

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -1,3 +1,5 @@
+
+LABEL=$1
 touch ~/.bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 source ~/.bash_profile
@@ -5,4 +7,4 @@ nvm install 4.0
 export PATH=$PATH:~/.nvm/versions/node/v4.0.0/bin
 npm install
 npm test
-tar --exclude='./tests' --exclude='./node_modules' --exclude='./README.md' --exclude='.gitignore' --exclude='./.git' --exclude='runUnitTests.sh'  -cvf QPG.tar .
+tar --exclude='./tests' --exclude='./node_modules' --exclude='./README.md' --exclude='.gitignore' --exclude='./.git' --exclude='runUnitTests.sh'  -cvf QPG-$LABEL.tar .
