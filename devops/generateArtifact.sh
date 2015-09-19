7cddd13aea05fdf34d1cc883c6e7c0b69fa535d5
dollyg <dollyg>
2015-09-19 15:35:58 +0530

    ignoring artifacts from the artifact.

--- devops/generateArtifact.sh
+++ devops/generateArtifact.sh
@@ -1,2 +1,2 @@
 LABEL=$1
-tar --exclude='./tests' --exclude='./node_modules' --exclude='./README.md' --exclude='.gitignore' --exclude='./.git' --exclude='./devops' --exclude='./data' -cvf QPG-$LABEL.tar .
+tar --exclude='./tests' --exclude='./node_modules' --exclude='./README.md' --exclude='.gitignore' --exclude='./.git' --exclude='./devops' --exclude='./data' --exclude='./QPG-Artifacts'  -cvf QPG-$LABEL.tar .
