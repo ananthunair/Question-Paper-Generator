544f287c662eb25520f890189e15f66b7fd3aec2
vjpratap <vijayprsingh619@gmail.com>
2015-09-23 17:27:54 +0530

    updating the UI of create question paper page

--- src/Preview/showPreview.js
+++ src/Preview/showPreview.js
@@ -2,7 +2,7 @@ var jade = require('jade');
 
 
 exports.show =function(questionPaper,screen){
-    var preview = jade.renderFile("./src/createQuestionPaper/preview.jade",{'questionPaper':questionPaper})
+    var preview = jade.renderFile("./src/Preview/preview.jade",{'questionPaper':questionPaper})
     var previewWindow = window.open("", "width=600,height=600,scrollbars=yes")
     previewWindow.focus();
     previewWindow.moveTo(0,0);
