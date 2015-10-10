7378ed0e530e7651abea8f3c59d3f756964fcb3b
vjpratap <vijayprsingh619@gmail.com>
2015-10-10 12:46:00 +0530

    [#48] Suparna/Vijay | UI changes for dashboard of user feedback on view all question papers

--- src/Dashboard/dashboard_presenter.js
+++ src/Dashboard/dashboard_presenter.js
@@ -5,11 +5,7 @@ exports.Presenter = function (view, questionPaperRepo) {
 
 exports.Presenter.prototype = {
     onDocumentReady : function(){
-        var questionPaper = {
-            id:1,
-            questions: [{id:111,note:"ppppp"}],
-            header:{title:"arraytest",marks:100,duration:"1 minute"}
-        };
+
         var view = this.view;
         var onComplete = function(err, questionPapers){
             err && console.log("Error while showing all question papers: ",err);
