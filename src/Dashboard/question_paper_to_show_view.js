a529f65c616276c69af3d9f1524556651534b226
vjpratap <vijayprsingh619@gmail.com>
2015-10-10 21:14:52 +0530

    [#49] | Suparna/Vijay | Implemented Preview of QuestionPaper

--- src/Dashboard/question_paper_to_show_view.js
+++ src/Dashboard/question_paper_to_show_view.js
@@ -2,11 +2,15 @@ var Presenter = require('./dashboard/question_paper_to_show_presenter.js').Prese
 var Question_papers_repository = require('./repository/question_paper_repo.js').Question_papers_repository;
 var Question_repository = require('./repository/create_question_repo.js').Question_repository;
 var preview = require('./preview/showPreview.js');
+var jade = require('jade');
 
 
 var view = {
     onQuestionPaperClick:function(setOfQuestions,title){
-        preview.show({title:title,'questions':setOfQuestions},screen)
+        //preview.show({title:title,'questions':setOfQuestions},screen)
+        console.log('=======view=======',setOfQuestions,'==========================',title);
+        var codeFormatedQuestions = jade.renderFile("./src/Preview/preview.jade",{title:title,'questions':setOfQuestions});
+        $('#question_paper_preview').html(codeFormatedQuestions)
     }
 };
 
@@ -19,7 +23,7 @@ $(document).ready(function (){
     $('#create_question_paper').click(function(){
         render('./src/createQuestionPaper/create_question_paper.jade')
     });
-    $('.questionPaperTitle').click(function(){
+    $('.question_paper_title').click(function(){
         var status = $(this).attr('id');
         presenter.getAllQuestionsFromPaper(status);
     });
