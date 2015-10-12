f8b449454b889956ea38675ec599b7ea55e111ed
suparna619 <suparnad@thoughtworks.com>
2015-10-12 19:56:44 +0530

    [#49] | Suparna | Property of Preview button is implemented

--- src/Dashboard/question_paper_to_show_view.js
+++ src/Dashboard/question_paper_to_show_view.js
@@ -3,14 +3,20 @@ var Question_papers_repository = require('./repository/question_paper_repo.js').
 var Question_repository = require('./repository/create_question_repo.js').Question_repository;
 var preview = require('./preview/showPreview.js');
 var jade = require('jade');
-var selectedid ;
+var setOfQuestionsOfPaper, titleOfPaper;
 
 var view = {
     onQuestionPaperClick:function(setOfQuestions,title,id){
+        setOfQuestionsOfPaper = setOfQuestions;
+        titleOfPaper = title;
         $(".question_paper_title").toggleClass('selected',false)
         $("#"+id).toggleClass('selected')
         var codeFormatedQuestions = jade.renderFile("./src/Dashboard/question_paper_preview.jade",{title:title,'questions':setOfQuestions});
         $('#question_paper_preview').html(codeFormatedQuestions)
+    },
+
+    openPreview: function () {
+        preview.show({title: titleOfPaper, 'questions': setOfQuestionsOfPaper}, screen)
     }
 };
 
@@ -27,6 +33,7 @@ $(document).ready(function (){
         var id = $(this).attr('id');
         presenter.getAllQuestionsFromPaper(id);
     });
-
-
+    $("#preview_button").click(function(){
+        view.openPreview();
+    });
 });
