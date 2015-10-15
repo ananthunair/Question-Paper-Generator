57c996333128010867980b053ca73de42bd045b7
dollyg <dollyg>
2015-10-15 21:12:17 +0530

    Fixing script for generate artefacts.

--- devops/generateArtifact.sh
+++ devops/generateArtifact.sh
@@ -4,4 +4,4 @@ zip -r app.nw *
 mv app.nw node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/
 mkdir -p QPG.app
 cp -r node_modules/nodewebkit/nodewebkit/node-webkit.app/* QPG.app/
-zip -r QPG-$LABEL.app QPG-LABEL.app/*
+zip -r QPG-$LABEL.app QPG.app/*
