c620a868af39ff028f621b433f42499f5df6bee7
vjpratap <vijayprsingh619@gmail.com>
2015-10-19 19:13:24 +0530

    [#53] Vijay/Ankur | fixed tags on show all questions page

--- src/BrowseAllQuestions/all_questions_presenter.js
+++ src/BrowseAllQuestions/all_questions_presenter.js
@@ -1,3 +1,6 @@
+var lodash  = require('lodash');
+
+
 exports.Presenter = function (view, questions_repo) {
     this.view = view;
     this.repo = questions_repo;
@@ -11,5 +14,27 @@ exports.Presenter.prototype = {
             presenter.view.showAllQuestions(questions);
         };
         this.repo.fetchQuestions(onComplete);
+    },
+    setAutosuggetions : function(){
+        var view = this.view;
+        this.repo.getUniqueTags(function (err,tags) {
+            if(!err)
+                view.setupTagBoxData(tags);
+        });
+    },
+    onAddOrRemoveTag :  function(tags){
+        var presenter =  this;
+        var onComplete = function(err,questions){
+            presenter.view.showAllQuestions(questions);
+            presenter.view.addQuestionSelectionListener();
+        };
+        this.repo.fetchQuestionsOfSpecificTags(tags,onComplete);
+    },
+    onNewQuestionAdded:function(){
+        var presenter =this;
+        this.onAddOrRemoveTag(this.view.getTags())
+        this.repo.getUniqueTags(function(err,tags){
+            presenter.view.addSuggetions(tags)
+        });
     }
 }
--- src/BrowseAllQuestions/all_questions_view.js
+++ src/BrowseAllQuestions/all_questions_view.js
@@ -8,15 +8,83 @@ var view = {
     showAllQuestions:function(questions){
         var htmlForQuestionsToSelect = jade.renderFile('./src/createQuestionPaper/questionToSelect.jade', {'questions': questions});
         $('#all_question_container').html(htmlForQuestionsToSelect)
+    },
+    setupTagBoxData: function(tags){
+        suggestedTag = tags;
+        this.tagBox = setupTagBox()
+    },
+    addQuestionSelectionListener: function () {
+        $('#tbl-questionsToSelect tbody').on('click', 'tr', function () {
+            $(this).toggleClass('selected');
+        });
+    },
+    getTags: function () {
+        return this.tagBox.getTagValues().slice(0);
+    },
+
+
+    addSuggetions:function(tags){
+        var newTags = lodash.difference(tags,suggestedTag);
+
+        newTags.forEach(function(tag){
+            suggestedTag.push(tag)
+        });
+        resettagBox(this.tagBox)
+        this.tagBox.resetAllowedTags(tags)
     }
+
 }
 
 var presenter = new Presenter(view, repo);
 
 $(document).ready(function(){
     presenter.onDocumentReady();
+    presenter.setAutosuggetions();
+
     $('#create_question').click(function(){
         var createQuestionPopUp =  jade.renderFile('./src/createQuestion/create_question.jade');
         $('#myModal').html(createQuestionPopUp);
+        $(".close").click(function () {
+            presenter.onNewQuestionAdded();
+        })
     });
 });
+
+var resettagBox =function(tagbox){
+    var container = tagbox.getContainer();
+    var input = tagbox.getInput();
+    $(input).autocomplete({
+        source:suggestedTag,
+        appendTo: container,
+        position: { at: 'left bottom', of: container },
+        select: function(e, v) {
+            e.preventDefault();
+            if (e.which === 1) {
+                tagbox.add(v.item.value);
+            }
+        }
+    });
+}
+
+
+var setupTagBox = function () {
+    var enteredtags = [];
+    var tagbox = new Taggle($('.tagbox.textarea')[0], {
+        duplicateTagClass: 'bounce',
+        allowedTags: suggestedTag,
+        cssclass:"searchTagBox",
+        placeholder:"Search Questions",
+        onTagAdd: function (event, tag) {
+            enteredtags.push(tag);
+            presenter.onAddOrRemoveTag(enteredtags);
+        },
+        onTagRemove: function (event, tag) {
+            lodash.remove(enteredtags, function (t) {
+                return t == tag
+            });
+            presenter.onAddOrRemoveTag(enteredtags);
+        }
+    });
+    resettagBox(tagbox)
+    return tagbox;
+}
