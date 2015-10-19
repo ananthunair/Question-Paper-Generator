a78ccf8bf6532ec85329db0a8e8b294dd07aaf20
suparna619 <deysuparnatw619@rediffmail.com>
2015-10-19 12:52:29 +0530

    [#72] | Suparna | Preview window not focussed by default - Issue fixed

--- src/Preview/showPreview.js
+++ src/Preview/showPreview.js
@@ -3,7 +3,9 @@ var jade = require('jade');
 
 exports.show =function(questionPaper,screen){
     var preview = jade.renderFile("./src/Preview/preview.jade",{'questionPaper':questionPaper})
-    var previewWindow = window.open("", "width=600,height=600,scrollbars=yes")
+    var previewWindow = window.open("width=600,height=600,scrollbars=yes", {
+        "focus": true
+    })
     previewWindow.focus();
     previewWindow.moveTo(0,0);
     previewWindow.resizeTo(screen.width, screen.height)
