e2a5c9fffd97e25911a2c1efa56e06a2c19103fe
dollyg <dollyg>
2015-10-23 15:32:34 +0530

    Removing app.nw only once

--- devops/generateArtifact.sh
+++ devops/generateArtifact.sh
@@ -5,6 +5,5 @@ zip -r app.nw *
 mv app.nw node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/
 mkdir -p QPG.app
 cp -r node_modules/nodewebkit/nodewebkit/node-webkit.app/* QPG.app/
-rm -rf node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/app.nw
 zip -r QPG-$LABEL.app QPG.app/*
 rm -rf QPG.app
