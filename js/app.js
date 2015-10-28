0eea6d07a9ae6de7678136f371bf05803a90db1b
Ananthu R V <ananthurv666@hotmail.com>
2015-10-28 13:42:07 +0530

    [#73] | Ananthu | window size fixed, removed scroll from tag box

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var gui = require('nw.gui');
 win = gui.Window.get();
 var nativeMenuBar = new gui.Menu({ type: "menubar" });
 try {
-    win.maximize();
     nativeMenuBar.createMacBuiltin("My App");
     win.menu = nativeMenuBar;
 } catch (ex) {
