4dcd7f973dbaa91004ea736ac8df67dcc3b1cbf1
dollyg <dollyg@thoughtworks.com>
2016-01-24 12:31:58 +0530

    [Sayli/Dolly] : Added js script for populating data in the DB which accepts a data json. A sample json is added for reference.

--- scripts/populateDB.js
+++ scripts/populateDB.js
@@ -1,33 +1,37 @@
-var dbName = "qpg_prod";
-var path = process.argv[2];
-
+var dataFile = process.argv[2];
 var fs = require('fs');
-var repo = require('../src/repo.js');
-var mongoose = require('mongoose');
-repo.connectDb(dbName);
+require('../src/repo.js').connectDb("qpg_prod");
 
-var Question = mongoose.model('Question');
-var QuestionPaper = mongoose.model('QuestionPaper');
-var Tags = mongoose.model('Tags');
+var Question_papers_repository = require('../src/repository/question_paper_repo.js').Question_papers_repository;
+var Question_repository = require('../src/repository/create_question_repo.js').Question_repository;
+var questionRepo = new Question_repository();
+var questionPaperRepo = new Question_papers_repository();
 
+var savedQuestions = [];
 
+var onData = function (err, data) {
+    if (err) return;
+    var dataJson = JSON.parse(data);
+    dataJson.questions.forEach(function (question) {
+        questionRepo.create(question, function (err, que) {
+            console.log("Question inserted");
+            savedQuestions.push(que);
+        });
+    });
 
-var onJSON = function(err, data){
-    var json = JSON.parse(data);
-    if(err) return;
+    dataJson.papers.forEach(function (paper) {
+        var questionsOfPaper = dataJson.questions.filter(function (que) {
+            return que.qPapers.indexOf(paper.header.title) != 0
+        });
 
-    var modelToDataHash = [
-        {model: Question, data: json.questions},
-        {model: QuestionPaper, data: json.questionPapers},
-        {model: Tags, data: json.tags}
-    ];
+        paper.questions = questionsOfPaper.map(function (q) {
+            return q._id;
+        });
 
-    modelToDataHash.forEach(function(row){
-       if(row.data.length == 0) return;
-       row.model.collection.insert(row.data, function(){
-           console.log(row.model.modelName + " collection updated.");
-       });
+        questionPaperRepo.saveQuestionPaper(paper, function (err, paper) {
+            console.log("Paper inserted.");
+        });
     });
 };
 
-fs.readFile(path, 'utf8', onJSON);
+fs.readFile(dataFile, 'utf8', onData);
