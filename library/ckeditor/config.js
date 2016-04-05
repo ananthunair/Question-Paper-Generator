6ca73a57b928eec1388bfcf34daba1fc8868c1a4
Ananthu R V <ananthurv666@hotmail.com>
2016-04-05 21:38:26 +0530

    [#95]|Ananthu| Added option to add code snippet and added way to see source, still to add autofocus and validate stil to add, remove markascode{WIP} - Ananthu R V

--- library/ckeditor/config.js
+++ library/ckeditor/config.js
@@ -24,10 +24,11 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'others' },
 		{ name: 'about' }
 	];
+	config.extraPlugins = 'sourcedialog,syntaxhighlight';
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript,Bold,About,Link,Italic,Unlink,BulletedList,NumberedList,Indent,Outdent';
 
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = 'link:advanced';
